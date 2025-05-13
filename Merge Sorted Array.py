6df6f7faba5c46665779c7022ca4d29cb51ae7f4
MilesGreen7 <speedymg7@gmail.com>
2025-05-12 23:58:59 -0700

    partial/incomplete solution

--- Merge Sorted Array.py
+++ Merge Sorted Array.py
@@ -3,4 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        if m == 0:
+            nums1 = nums2
+            return
+        if n == 0:
+            return
+
+        index1 = 0
+        index2 = 0
+        indexInsert = m
+
+
         
