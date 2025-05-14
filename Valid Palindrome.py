b0807a89d1e794fb1e31d442916f24f18caa43d6
MilesGreen7 <speedymg7@gmail.com>
2025-05-14 11:38:20 -0700

    finished palindrome exercise

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -1,3 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+        index1 = 0
+        index2 = len(s) - 1
+        while index1 < index2:
+            while index1 < index2 and not s[index1].isalnum():
+                index1 += 1
+                
+            while index2 > index1 and not s[index2].isalnum():
+                index2 -= 1
+            
+            if s[index1].lower() != s[index2].lower():
+                return False
+            index1 += 1
+            index2 -= 1
+            
