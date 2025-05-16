69cab236afe20ee0d171f6f99e2e3bd245ce9205
MilesGreen7 <speedymg7@gmail.com>
2025-05-16 00:11:12 -0700

    optimized solution for is subsequence

--- Is Subsequence.py
+++ Is Subsequence.py
@@ -1,12 +1,11 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i = 0
-        for c in t:
-            if i < len(s):
-                if c == s[i]:
-                    i += 1
-            else:
-                return True
-        if i >= len(s):
+        if len(s) == 0:
             return True
+        i = 0
+        for c in t:     
+            if c == s[i]:
+                i += 1
+            if i >= len(s):
+                return True   
         return False
