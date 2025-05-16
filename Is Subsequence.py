1b1cb4496b1be32ff5f59cd1a82e6956a9a4cf82
MilesGreen7 <speedymg7@gmail.com>
2025-05-16 00:04:30 -0700

    solution to is subsequence

--- Is Subsequence.py
+++ Is Subsequence.py
@@ -1,3 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        
+        i = 0
+        for c in t:
+            if i < len(s):
+                if c == s[i]:
+                    i += 1
+            else:
+                return True
+        return False
