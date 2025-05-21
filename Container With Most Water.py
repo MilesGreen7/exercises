c21ae54e8013108e083c962d0db0220d661a8637
MilesGreen7 <speedymg7@gmail.com>
2025-05-20 23:33:43 -0700

    Finished Container with Most Water Exercise

--- Container With Most Water.py
+++ Container With Most Water.py
@@ -1,3 +1,11 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
+        maxWater = 0
+        leftPtr = 0
+        rightPtr = len(height) - 1
+        while leftPtr < rightPtr:
+            tempWater = min(height[leftPtr], height[rightPtr]) * (rightPtr - leftPtr)
+            maxWater = max(maxWater, tempWater)
+            if height[leftPtr] > height[rightPtr]:
+                rightPtr -= 1
+        return maxWater
