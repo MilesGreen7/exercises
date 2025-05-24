b4b2730cbf2e3973b1f302dc5d8687571551ee88
MilesGreen7 <speedymg7@gmail.com>
2025-05-23 23:56:14 -0700

    finished alternate implementation for ransome note exercise

--- Ransom Note Alternate Implementation.py
+++ Ransom Note Alternate Implementation.py
@@ -1,2 +1,23 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        index = 0
+        letters = {}
+        for c in magazine:
+            if c == ransomNote[index]:
+                index += 1
+                if index == len(ransomNote):
+                    return True
+                continue
+            if c not in letters.keys():
+                letters[c] = 1
+            else:
+                letters[c] += 1
+        while index != len(ransomNote):
+            if ransomNote[index] not in letters.keys():
+                return False
+            letters[ransomNote[index]] -= 1
+            if letters[ransomNote[index]] < 0:
+                return False
+            index += 1
+            
+        return True
