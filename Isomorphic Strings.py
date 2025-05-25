1afe46dcf46168df07fc1ffa0cec720e35efdd63
MilesGreen7 <speedymg7@gmail.com>
2025-05-25 14:30:00 -0800

    finished isomorphic strings

--- Isomorphic Strings.py
+++ Isomorphic Strings.py
@@ -1,2 +1,12 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        charDict = {}
+        for i in range(len(s)):
+            if s[i] in charDict.keys():
+                if charDict[s[i]] != t[i]:
+                    return False
+            elif t[i] in charDict.values():
+                return False
+            else:
+                charDict[s[i]] = t[i]
+        return True
