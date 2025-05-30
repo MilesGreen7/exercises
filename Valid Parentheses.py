1236397205787e0ff250e5967249c3bcdbbb0b3e
MilesGreen7 <speedymg7@gmail.com>
2025-05-29 23:54:57 -0700

    valid parentheses exercise finished

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,2 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        arr = []
+        for c in s:
+            if c is ']':
+                if len(arr) == 0 or arr.pop(-1) != '[':
+                    return False
+            elif c is ')':
+                if len(arr) == 0 or arr.pop(-1) != '(':
+                    return False
+            elif c is '}':
+                if len(arr) == 0 or arr.pop(-1) != '{':
+                    return False
+            else:
+                arr.append(c)
+        if len(arr) == 0:
+            return True
